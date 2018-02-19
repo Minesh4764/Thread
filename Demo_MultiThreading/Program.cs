9624dd324cef8d694e22310d333590bf113a6cb9
mineshpatel20872 <mineshpatel20872@hotmail.com>
2018-02-19 16:10:24 -0500

    Add project files.

---
+++ Demo_MultiThreading/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Demo_MultiThreading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ThreadStart th1 = new ThreadStart(f1);
+            ThreadStart th2 = new ThreadStart(f2);
+
+            Thread t1 = new Thread(th1);
+            Thread t2 = new Thread(th2);
+
+            t1.Start();     // Initializing Threads into ThreadPool
+            t2.Start();
+
+        }
+
+        static void f1()
+        {
+            for(int i=1;i<=20;i++)
+            {
+                Thread.Sleep(50);
+                Console.WriteLine("Inside F1 ......." + i);
+            }
+        }
+
+        static void f2()
+        {
+            for (int i = 1; i <= 20; i++)
+            {
+                Thread.Sleep(50);
+                Console.WriteLine("Inside F2 ......." + i);
+            }
+        }
+    }
+}
